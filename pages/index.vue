ce89ccc69558794a65f70e00b76d2ec620c85820
alishercpp <alisher.abdimuminov.2005@gmail.com>
2025-02-19 12:04:09 +0500

    bug fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,63 @@
 <script setup lang="ts">
-import { MainButton, useWebAppPopup, useMiniApp } from 'vue-tg'
+import { LucideChevronRight, LucideFlaskConical, LucideMessageCircleQuestion, LucideWallet } from 'lucide-vue-next';
+import { useMiniApp } from 'vue-tg'
 
 
-const { showAlert } = useWebAppPopup();
 const miniApp = useMiniApp();
 
-console.log(miniApp);
 
-const getAuth = async () => {
-    let response = await $fetch("/api/auth");
-    console.log(response);
-}
-
-onMounted(() => {
-    console.log("Response", getAuth());
+const user = computed(() => {
+    if (miniApp.initDataUnsafe.user) {
+        if (miniApp.initDataUnsafe.user.first_name) {
+            return miniApp.initDataUnsafe.user.first_name;
+        } else if (miniApp.initDataUnsafe.user.username) {
+            return miniApp.initDataUnsafe.user.username;
+        } else {
+            return miniApp.initDataUnsafe.user.id;
+        }
+    } else {
+        return "User";
+    }
 });
+
 </script>
 
 <template>
-    <div>
-        <ClientOnly>
-            <MainButton text="Open alert" @click="showAlert('Hello')" />
-        </ClientOnly>
+    <div class="h-screen w-full bg-gradient-to-r from-orange-500 via-yellow-500 to-yellow-500">
+        <div class="h-[12rem] p-5">
+            <p>Hello {{ user }}</p>
+            <p class="text-3xl">Welcome to Astron</p>
+        </div>
+        <div class="h-[calc(100%-12rem)] flex flex-col gap-2 bg-background border-t rounded-t-3xl p-5">
+            <div class="bg-accent/30 rounded-md divide-y">
+                <div class="flex justify-between p-3">
+                    <div class="flex items-center gap-2">
+                        <LucideMessageCircleQuestion :size="20" />
+                        <p>Savollar</p>
+                    </div>
+                    <div class="flex items-center justify-center">
+                        <LucideChevronRight />
+                    </div>
+                </div>
+                <div class="flex justify-between p-3">
+                    <div class="flex items-center gap-2">
+                        <LucideFlaskConical :size="20" />
+                        <p>Testlar</p>
+                    </div>
+                    <div class="flex items-center justify-center">
+                        <LucideChevronRight />
+                    </div>
+                </div>
+                <div class="flex justify-between p-3">
+                    <div class="flex items-center gap-2">
+                        <LucideWallet :size="20" />
+                        <p>Testlar</p>
+                    </div>
+                    <div class="flex items-center justify-center">
+                        <LucideChevronRight />
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
