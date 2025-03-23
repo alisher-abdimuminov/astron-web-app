1b525f00e618ae60697505e1315b3e6a90ffd3b9
Ali <alisher.abdimuminov.2005@gmail.com>
2025-03-23 23:24:12 +0500

    bug fix

--- db.ts
+++ db.ts
@@ -1,20 +1,18 @@
 import { readFileSync, writeFileSync, existsSync } from "fs";
 import path from "path";
 
-const DB_FILE = path.resolve("server/db.json");
 
-// Sana formatini `dd-mm-yyyy` shaklga o‘tkazish
 const formatDate = (date: Date) => {
     return date.toLocaleDateString("en-GB").replace(/\//g, "-");
 };
 
 const read = (): { date: string; viewers: number } => {
-    if (!existsSync(DB_FILE)) {
+    if (!existsSync("db.json")) {
         return { date: formatDate(new Date()), viewers: 0 };
     }
 
     try {
-        const data = JSON.parse(readFileSync(DB_FILE, "utf-8"));
+        const data = JSON.parse(readFileSync("db.json", "utf-8"));
         const today = formatDate(new Date());
 
         // Agar sana eski bo‘lsa, 0 dan boshlaymiz
@@ -30,7 +28,7 @@ const read = (): { date: string; viewers: number } => {
 
 const write = (viewers: number) => {
     const data = { date: formatDate(new Date()), viewers };
-    writeFileSync(DB_FILE, JSON.stringify(data, null, 4), "utf-8"); // JSON formatlash
+    writeFileSync("db.json", JSON.stringify(data, null, 4), "utf-8"); // JSON formatlash
 };
 
 export { read, write };
