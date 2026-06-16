fe0ce16dd106d7ef0cb6b0f96e9847307c1ec10f
Ali <alisher.abdimuminov.2005@gmail.com>
2026-06-16 23:14:06 +0500

    bug fix

--- app.vue
+++ app.vue
@@ -1,25 +1,29 @@
 <script setup lang="ts">
 onMounted(() => {
-    if (window.yaContextCb !== null) {
-        window.yaContextCb.push(() => {
-            Ya.Context.AdvManager.render({
-                blockId: "R-A-19395198-2",
-                type: "fullscreen",
-                platform: "touch",
-            });
-        });
-    }
+	if (
+		window.yaContextCb ||
+		window.yaContextCb !== null ||
+		window.yaContextCb !== undefined
+	) {
+		window.yaContextCb.push(() => {
+			Ya.Context.AdvManager.render({
+				blockId: "R-A-19395198-2",
+				type: "fullscreen",
+				platform: "touch",
+			});
+		});
+	}
 });
 </script>
 
 <template>
-    <div>
-        <NuxtRouteAnnouncer />
-        <NuxtLoadingIndicator />
-        <NuxtLayout>
-            <ClientOnly>
-                <NuxtPage />
-            </ClientOnly>
-        </NuxtLayout>
-    </div>
+	<div>
+		<NuxtRouteAnnouncer />
+		<NuxtLoadingIndicator />
+		<NuxtLayout>
+			<ClientOnly>
+				<NuxtPage />
+			</ClientOnly>
+		</NuxtLayout>
+	</div>
 </template>
